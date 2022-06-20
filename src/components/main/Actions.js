8c9a98bb798c257de97ab470103ea4cbf652e6b8
marta-dot <74323198+marta-dot@users.noreply.github.com>
2022-06-20 13:15:41 +0200

    Added action search

--- src/components/main/Actions.js
+++ src/components/main/Actions.js
@@ -1,11 +1,21 @@
 import actions from "../actions";
 import Action from "../Action";
+import {useState} from "react";
 
 const Actions = () => {
-  return(
-    <div>
-      {actions.map(action => <Action key={action.name} name={action.name} cost={action.cost} />)}
-    </div>
+    const [filter, setFilter] = useState("");
+
+
+    return(
+        <div>
+        <input className={"search"} placeholder="Search for Action" onChange={e => setFilter(e.currentTarget.value.toLowerCase())} type={"text"}/>
+
+            {actions.map(action => {
+                console.log(action.name);
+                if(action.name.toLowerCase().includes(filter))
+                return <Action key={action.name} name={action.name} cost={action.cost} />
+            })}
+        </div>
   )
 }
 export default Actions;
